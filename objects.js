d9b9a1472d5ea2e0d50366e023ed0124b43ad815
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-11 23:20:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,14 @@ var playlist = {
   Bayside : 'Demons',
   Reol : 'Drop Pop Candy'
 };
-
+/*
 function updatePlaylist(playlistName, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
+}
+*/
+function updatePlaylist(playlistName, artistName, songTitle){
+  return Object.assign(playlist, playlist, {[artistName]: songTitle});
 }
 
 function removeFromPlaylist(playlist, artistName){
