8aaaa085603ba9e7b2f128fa9d7ec916fa9ed825
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-11 22:43:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ const recipe = {eggs: 3};
 destructivelyUpdateObjectWithKeyAndValue(recipe, 'eggs', 'sausage');
 
 //the above will return our constant variable that was actually changed.
+
+function updateObjectWithKeyAndValue(obj, key, value){
+  Object.assign({}, obj, {[key]: value});
+  // remember to use brackets
+}
